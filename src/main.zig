620e6305bc13b8f280a3d870aad87f4a29959de8
redstonetrail <joe.bearchell@gmail.com>
2025-06-05 09:11:53 +0100

    improved algorithm, can run indefinately

--- src/main.zig
+++ src/main.zig
@@ -12,7 +12,19 @@ const Stream = struct {
     current_row: u16,
     length: u16,
     finished: bool,
-    last_character: []u8,
+    last_character: u8,
+};
+
+const characters = blk: {
+    @setEvalBranchQuota(100000);
+
+    var ascii_chars: [93][1]u8 = undefined;
+
+    for (0..93) |i| {
+        ascii_chars[i][0] = i + 33;
+    }
+
+    break :blk ascii_chars;
 };
 
 // // only want characters from hex 21 to 7e (dec 33 to 126) as they are ascii and continuous in number
@@ -35,10 +47,10 @@ pub fn main() !void {
     const allocator = arena.allocator();
 
     // vaxis
-    var tty = try vaxis.Tty.init();
+    var tty = vaxis.Tty.init() catch std.debug.panic("could not init tty", .{});
     defer tty.deinit();
 
-    var vx = try vaxis.init(allocator, .{});
+    var vx = vaxis.init(allocator, .{}) catch std.debug.panic("could not init vaxis", .{});
     defer vx.deinit(allocator, tty.anyWriter());
 
     var event_loop: vaxis.Loop(Event) = .{
@@ -46,40 +58,49 @@ pub fn main() !void {
         .vaxis = &vx,
     };
 
-    try event_loop.start();
+    event_loop.start() catch std.debug.panic("could not start event loop", .{});
     defer event_loop.stop();
 
-    try vx.enterAltScreen(tty.anyWriter());
+    vx.enterAltScreen(tty.anyWriter()) catch std.debug.panic("could not enter alt screen", .{});
 
-    try vx.queryTerminal(tty.anyWriter(), std.time.ns_per_s);
+    vx.queryTerminal(tty.anyWriter(), std.time.ns_per_s) catch std.debug.panic("could not query terminal", .{});
 
     {
         const event = event_loop.nextEvent();
         switch (event) {
-            .winsize => |ws| try vx.resize(allocator, tty.anyWriter(), ws),
+            .winsize => |ws| vx.resize(allocator, tty.anyWriter(), ws) catch std.debug.panic("could not do initial resize", .{}),
             else => {},
         }
     }
 
     {
         var tty_bw = tty.bufferedWriter();
-        try vx.render(tty_bw.writer().any());
-        try tty_bw.flush();
+        vx.render(tty_bw.writer().any()) catch std.debug.panic("could not render screen", .{});
+        tty_bw.flush() catch std.debug.panic("could not flush screen", .{});
     }
 
-    var running: u8 = 3;
+    var running: u8 = 3; // magic number, multiple "key presses" are always sent, so we count down from 3
 
-    var streams: [100]Stream = undefined;
+    var streams: []Stream = allocator.alloc(Stream, vx.window().width * vx.window().height / 2) catch std.debug.panic("oom on initial streams alloc", .{});
 
-    for (0..(streams.len - 1)) |current_stream| {
+    for (0..(streams.len / 2 - 1)) |current_stream| {
         streams[current_stream] = .{
-            .column = random.intRangeLessThan(u16, 0, vx.window().width - 1),
-            .length = random.intRangeLessThan(u16, 0, vx.window().height - 1),
-            .current_row = random.intRangeLessThan(u16, 0, vx.window().height - 1),
-            .finished = false,
-            .last_character = @constCast(" "),
+            .column = 0,
+            .length = 3,
+            .current_row = 0,
+            .finished = true,
+            .last_character = 0,
         };
     }
+    // for (0..(streams.len / 2 - 1)) |current_stream| {
+    //     streams[current_stream] = .{
+    //         .column = random.intRangeLessThan(u16, 0, vx.window().width - 1),
+    //         .length = random.intRangeLessThan(u16, 0, vx.window().height - 1),
+    //         .current_row = random.intRangeLessThan(u16, 0, vx.window().height - 1),
+    //         .finished = false,
+    //         .last_character = 0,
+    //     };
+    // }
 
     // main loop
     while (running >= 1) {
@@ -87,12 +108,11 @@ pub fn main() !void {
         var tty_bw = tty.bufferedWriter();
 
         for (0..(streams.len - 1)) |current_stream| {
-            var current_character: [1]u8 = undefined;
-            current_character[0] = random.intRangeLessThan(u8, 33, 126);
+            const random_number = random.intRangeLessThan(u8, 0, 93);
 
             _ = window.print(&.{
                 .{
-                    .text = try allocator.dupe(u8, &current_character),
+                    .text = &characters[random_number],
                     .style = vaxis.Style{
                         .bold = true,
                         .fg = .{
@@ -106,19 +126,12 @@ pub fn main() !void {
                 .col_offset = streams[current_stream].column,
             });
 
-            // const green_coefficient = random.intRangeLessThan(u8, 1, 5);
-
             _ = window.print(&.{
                 .{
-                    .text = try allocator.dupe(u8, streams[current_stream].last_character),
+                    .text = &characters[random_number],
                     .style = vaxis.Style{
                         .bold = false,
                         .fg = .{
-                            // .rgb = .{
-                            //     0x00,
-                            //     0xAA / green_coefficient,
-                            //     0x00,
-                            // },
                             .index = 2,
                         },
                     },
@@ -141,7 +154,7 @@ pub fn main() !void {
                 },
             });
 
-            streams[current_stream].last_character = &current_character;
+            streams[current_stream].last_character = random_number;
             streams[current_stream].current_row += 1;
         }
 
@@ -162,18 +175,25 @@ pub fn main() !void {
         try tty_bw.flush();
 
         for (0..(streams.len - 1)) |current_stream| {
-            if (streams[current_stream].current_row == (window.height + 1)) {
+            if (streams[current_stream].current_row == (window.height + streams[current_stream].length + 2)) {
                 streams[current_stream].finished = true;
+                break;
             }
+        }
 
-            if (streams[current_stream].finished) {
-                streams[current_stream] = .{
-                    .length = random.intRangeLessThan(u16, 0, (window.height - 1) * 2 / 3),
-                    .column = random.intRangeLessThan(u16, 0, window.width - 1),
-                    .current_row = 0,
-                    .finished = false,
-                    .last_character = @constCast(" "),
-                };
+        for (0..4) |_| {
+            for (0..(streams.len - 1)) |current_stream| {
+                if (streams[current_stream].finished) {
+                    streams[current_stream] = .{
+                        .length = random.intRangeLessThan(u16, 3, (window.height - 1) * 2 / 3),
+                        .column = random.intRangeLessThan(u16, 0, window.width - 1),
+                        // .current_row = random.intRangeLessThan(u16, 0, (window.height - 1) / 3),
+                        .current_row = 0,
+                        .finished = false,
+                        .last_character = 0,
+                    };
+                    break;
+                }
             }
         }
 
